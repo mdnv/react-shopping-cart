90ccc906ede49fe6ee6db790375392a8c6cd663c
manhnd3 <manhnd@2nf.com.vn>
2020-02-17 14:40:09 +0700

    config to get api from sample_app

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-const port = 8001;
+const port = 3001;
 
 app.get('/api/products', (req, res) => {
   res.sendFile(path.join(__dirname, 'data', 'products.json'));
--- src/components/Shelf/ProductList/Product/index.js
+++ src/components/Shelf/ProductList/Product/index.js
@@ -38,7 +38,7 @@ const Product = ({ product, addProduct }) => {
       )}
       <Thumb
         classes="shelf-item__thumb"
-        src={require(`../../../../static/products/${product.sku}_1.jpg`)}
+        src={product.image_url}
         alt={product.title}
       />
       <p className="shelf-item__title">{product.title}</p>
